1c8929151bdef28a740f1e2b6ca07e169cae2c32
ioanapele43 <93875206+ioanapele43@users.noreply.github.com>
2022-03-08 09:40:24 +0200

    update

---
+++ src/week1/day1/lab3/TestMyPoint.java
@@ -0,0 +1,10 @@
+public class TestMyPoint {
+    public static void main(String[] args){
+        MyPoint point1=new MyPoint(1,2,3);
+        MyPoint point2=new MyPoint(3,4,5);
+        System.out.println("Distance by coordonates: ("+point1.getX()+","+point1.getY()+","+point1.getZ()+") and (5,5,5)");
+        System.out.println(point1.distance(5,5,5));
+        System.out.println("Distance between 2 points:("+point1.getX()+","+point1.getY()+","+point1.getZ()+") and("+point2.getX()+","+point2.getY()+","+point2.getZ()+")");
+        System.out.println(point1.distance(point2));
+    }
+}
--- src/week1/day1/lab3/VendingMachine.java
+++ src/week1/day1/lab3/VendingMachine.java
@@ -1,4 +1,20 @@
-package week1.day1.lab3;
+import java.util.HashMap;
 
 public class VendingMachine {
+    private int credit;
+    public void displayProducts(){
+
+    }
+    public void insertCoin(int value){
+
+    }
+    public String selectProduct(int id){
+        return "";
+    }
+    public void displayCredit(){
+
+    }
+    public void userMenu(){
+
+    }
 }
