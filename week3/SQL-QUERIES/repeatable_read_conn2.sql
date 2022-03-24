659dd030f8c4486d8efe508df48feb234c24334b
ioanapele43 <peleioana43@yahoo.com>
2022-03-24 11:20:31 +0200

    update

--- week3/SQL-QUERIES/repeatable_read_conn2.sql
+++ week3/SQL-QUERIES/repeatable_read_conn2.sql
@@ -1,7 +1,5 @@
 SET TRANSACTION ISOLATION LEVEL repeatable read;
 
-	do sleep(5);
-    
 	insert into shows ( title, director, releasedate,lastmodification) 
     values ("r2","aaaA",now(),now());
     
