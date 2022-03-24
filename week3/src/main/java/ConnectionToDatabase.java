f4a62a9e23bdc20a29e948e3545e5e4a9c97ec0d
ioanapele43 <peleioana43@yahoo.com>
2022-03-24 05:02:43 +0200

    update

--- week3/src/main/java/ConnectionToDatabase.java
+++ week3/src/main/java/ConnectionToDatabase.java
@@ -1,36 +1,28 @@
 import java.sql.*;
 
 public class ConnectionToDatabase {
-    public static void main(String[] args) {
-        /*try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("All good.");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
-
-        Connection connection = null;
+    Connection connection = null;
+    public void seeShowsTable(){
         try {
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/arobs-internship", "student", "pele");
-           /* if (connection != null) {
-                System.out.println("Connection ok.");
+            if (connection != null) {
+                System.out.println("Connection ok. select");
                 Statement statement = connection.createStatement();
-                String query = "SELECT * FROM artists";
+                String query = "SELECT * FROM shows";
                 ResultSet rs = statement.executeQuery(query);
 
                 while (rs.next()) {
-                    int id = rs.getInt("id");
-                    String stageName = rs.getString("stage_name");
-                    String firstName = rs.getString("first_name");
-                    String lastName = rs.getString("last_name");
-                    String email = rs.getString("email");
-                    Date birthday = rs.getDate("birthday");
+                    int id = rs.getInt("idshow");
+                    String title = rs.getString("title");
+                    String director = rs.getString("director");
+                    Date releaseDate = rs.getDate("releasedate");
+                    Date lastModification = rs.getDate("lastModification");
 
-                    System.out.println(String.join(", ", String.valueOf(id), stageName, firstName, lastName, email, birthday.toString()));
+                    System.out.println(String.join(", ", String.valueOf(id), title,director,releaseDate.toString(),lastModification.toString()));
                 }
             } else {
                 System.out.println("Connection NOT ok.");
-            }*/
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
@@ -40,5 +32,83 @@ public class ConnectionToDatabase {
                 e.printStackTrace();
             }
         }
+    }
+    public void insertShowsTable(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/arobs-internship", "student", "pele");
+            if (connection != null) {
+                System.out.println("Connection ok.insert");
+                Statement statement = connection.createStatement();
+                String query = "insert into shows(idshow,title,director,releasedate) values (3,'aaa','ddd',now());";
+                boolean rs=statement.execute(query);
+
+
+            } else {
+                System.out.println("Connection NOT ok.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void updateShowsTable(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/arobs-internship", "student", "pele");
+            if (connection != null) {
+                System.out.println("Connection ok. update");
+                Statement statement = connection.createStatement();
+                String query = "update shows set director='ddddd' where idshow=3;";
+                boolean rs=statement.execute(query);
+
+
+            } else {
+                System.out.println("Connection NOT ok.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void deleteShowsTable(){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/arobs-internship", "student", "pele");
+            if (connection != null) {
+                System.out.println("Connection ok. delete ");
+                Statement statement = connection.createStatement();
+                String query = "delete from shows where idshow=3";
+                boolean rs=statement.execute(query);
+
+
+            } else {
+                System.out.println("Connection NOT ok.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        ConnectionToDatabase connectionToDatabase=new ConnectionToDatabase();
+        //connectionToDatabase.seeShowsTable();
+        connectionToDatabase.insertShowsTable();
+        connectionToDatabase.updateShowsTable();
+        connectionToDatabase.seeShowsTable();
+        connectionToDatabase.deleteShowsTable();
+        connectionToDatabase.seeShowsTable();
     }
 }
