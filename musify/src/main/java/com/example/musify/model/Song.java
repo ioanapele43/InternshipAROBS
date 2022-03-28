e18ab3322697af926ee056d648213661ac6a271f
ioanapele43 <peleioana43@yahoo.com>
2022-03-28 23:05:53 +0300

    update

---
+++ musify/src/main/java/com/example/musify/model/Song.java
@@ -0,0 +1,10 @@
+package com.example.musify.model;
+
+import java.util.List;
+
+public class Song {
+    private String title;
+    private String alternativeTitle;
+    private List<Artist> contributorArtist;
+    
+}
--- musify/src/main/java/com/example/musify/model/User.java
+++ musify/src/main/java/com/example/musify/model/User.java
@@ -1,4 +1,66 @@
 package com.example.musify.model;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String country;
+    private String role;
+    public User(String firstName,String lastName,String email,String password,String country,String role){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+        this.password=password;
+        this.country=country;
+        this.role=role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
