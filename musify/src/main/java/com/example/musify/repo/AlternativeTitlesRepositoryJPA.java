b0a60ad2c7f5be226207d76b1007cf7142ae6baa
ioanapele43 <93875206+ioanapele43@users.noreply.github.com>
2022-04-26 00:41:29 +0300

    update

--- musify/src/main/java/com/example/musify/repo/AlternativeTitlesRepositoryJPA.java
+++ musify/src/main/java/com/example/musify/repo/AlternativeTitlesRepositoryJPA.java
@@ -12,4 +12,5 @@ import java.util.Optional;
 public interface AlternativeTitlesRepositoryJPA extends JpaRepository<AlternativeTitles, Integer> {
     AlternativeTitles getAlternativeTitlesById(Integer id);
     List<AlternativeTitles> getAlternativeTitlesBySongId(Integer id);
+    
 }
