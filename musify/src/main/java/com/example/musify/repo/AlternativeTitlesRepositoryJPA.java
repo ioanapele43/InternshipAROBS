510dfe95461038508ac204290407d1e3c10bc293
ioanapele43 <93875206+ioanapele43@users.noreply.github.com>
2022-04-26 00:37:26 +0300

    update

--- musify/src/main/java/com/example/musify/repo/AlternativeTitlesRepositoryJPA.java
+++ musify/src/main/java/com/example/musify/repo/AlternativeTitlesRepositoryJPA.java
@@ -1,12 +1,15 @@
 package com.example.musify.repo;
 
+import com.example.musify.model.AlternativeTitles;
 import com.example.musify.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AlternativeTitlesRepositoryJPA extends JpaRepository<Song, Integer> {
-    Optional<Song> findById(Integer id);
+public interface AlternativeTitlesRepositoryJPA extends JpaRepository<AlternativeTitles, Integer> {
+    AlternativeTitles getAlternativeTitlesById(Integer id);
+    List<AlternativeTitles> getAlternativeTitlesBySongId(Integer id);
 }
